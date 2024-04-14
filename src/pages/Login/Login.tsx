4d688bf97ed8386fc360157ba5c93dbe8be8f3d9
Arturo J. Alva <arturo.j.a.m@outlook.com>
2024-04-13 23:09:49 -0500

    feature: add tailwind css in the project.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,8 +5,13 @@ const Login: FC = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <h1>Login</h1>
-      <button onClick={() => navigate("/home")}>Ir a Home</button>
+      <h1 className="text-4xl text-center">Login</h1>
+      <button
+        onClick={() => navigate("/home")}
+        className="px-5 py-2.5 text-sm font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Ir a Home
+      </button>
     </div>
   );
 };
